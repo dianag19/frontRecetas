3496b520e92aadd9b0a9761c4a96f8e0b596eea8
Diana <diana.gomez.caicedo@correounivalle.edu.co>
2019-10-26 06:14:15 -0500

    Upload project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatCardModule,
+    NgbModule,
+    MatGridListModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
+      
+    ]),
+    BrowserAnimationsModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductDetailsComponent,
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
---
+++ src/app/products.ts
@@ -0,0 +1,119 @@
+export const products = [
+  {
+    name: 'Postres fríos de galletas con Cremosino Alpina',
+    time: 30,
+    ingredients: ['3 tazas de galletas negras',
+      '½ taza de Mantequilla Alpina',
+      '2 tazas de Cremosino Alpina',
+      '1 taza de leche condensada',
+      '1 cucharadita de vainilla',
+      '2 cucharadas de gelatina sin sabor',
+      '⅓ taza de agua',
+      '1 taza de frutos rojos',
+    ],
+    preparation: 'Hidratamos la gelatina sin sabor en el agua por 3 minutos. Calentamos en el microondas y reservamos.' +
+      'Procesamos 3 galletas hasta volverlas polvo y reservamos 1 taza. A la otra le adicionamos la mantequilla alpina derretida y procesamos hasta que se forme una arenita. Reservamos.' +
+      'En una olla adicionamos el Cremosino Alpina con la leche condensada y la vainilla. Revolvemos hasta que todos los ingredientes se hayan derretido y adicionamos a la mezcla la gelatina sin sabor.' +
+      'Tomamos un molde para hacer tortas y en el fondo ponemos un poco de papel parafinado o papel chicle. Creamos una base con las galletas trituradas y adicionamos la mezcla de Cremosino Alpina.' +
+      'En la parte de arriba adicionamos el resto de las galletas trituradas. Llevamos a refrigeración por 4 horas como mínimo antes de cortarlo.' +
+      'Sacamos y servimos con los frutos rojos por encima.',
+    image: 'https://my7.digitalexperience.ibm.com/1285e1d2-5151-4eab-9da2-775291879cb9/dxresources/6edc/6edc383c-7447-413a-8ca5-8cab37172a1b.JPG',
+    description: 'Postre rápido y fácil de galletas que no requiere el uso de horno. Ideal para realizar junto a los más pequeños de la casa'
+  },
+  {
+    name: 'Anillos de buñuelos',
+    time: 30,
+    ingredients: ['1/2 libra de masa para buñuelos preparada',
+      '1/4 de taza de bocadillo cortado en cubos medianos',
+      '1/2 Queso Mozzarella Alpina en bloque',
+      'Aceite para freír'],
+    preparation: 'Hacemos bolitas de la masa de buñuelo y las rellenamos con el bocadillo y el Queso Mozzarella Alpina cortado en cubos medianos.' +
+      'Pegamos 5 bolitas formando un aro y las ponemos sobre papel parafinado, dándole la forma del aro para que no se separen en fritura.' +
+      'Llevamos al aceite a temperatura media baja hasta que los buñuelos estén dorados.' +
+      'Sacamos, dejamos reposar unos minuticos antes de retirar el papel y lo desprendemos suavemente.' +
+      'Servimos los anillos de buñuelo con chocolate caliente. ',
+    image: 'https://my7.digitalexperience.ibm.com/1285e1d2-5151-4eab-9da2-775291879cb9/dxresources/1cef/1cefd059-97af-4991-952c-e7548fdc40a8.jpg?resize=1400px%3A788px&crop=1400%3A500%3B0%2C144',
+    description: 'Dale un nuevo toque a esos tradicionales buñuelos y sorprende a toda la familia.'
+  },
+  {
+    name: 'Parfait de Bon Yurt y Mermelada',
+    time: 10,
+    ingredients: ['1 Bon Yurt de zucaritas', '4 cerezas', '4 cucharadas de Mermelada Alpina de fresas', '½ cucharada de chips de chocolate'],
+    preparation:'En un vaso grande o frasco servimos Mermelada de Fresas en el fondo.'+
+    'Destapamos el Bon Yurt de Zucaritas y ponemos el líquido en el frasco.'+
+    'Adicionamos los chips de chocolate y las Zucaritas en la parte superior.'+
+    'Agregamos las cerezas. Listo para mezcla, comer y una tarde de pelis de otro nivel.',
+    image: 'https://my7.digitalexperience.ibm.com/1285e1d2-5151-4eab-9da2-775291879cb9/dxresources/1010/101055c6-b038-4813-aa3a-dc8d8fd23d4c.jpg?resize=1400px%3A787px&crop=1400%3A500%3B0%2C14',
+    description: 'Para que una tarde de pelis se vuelva más divertida prepara esta receta facilísima de parfait de mermelada y Bonyurt Alpina. así que ¡Luces, cámara, acción!'
+  },
+  {
+    name: 'Batido de cheesecake con Cremosino Alpina',
+    time: 20,
+    ingredients: ['1 taza de hielo', '1 taza de frutos rojos', '4 galletas saladas', '1 taza de Súper Leche Actilife Alpina','1 cucharada de Cremosino Alpina','1 cucharadita de extracto de vainilla','⅓ de taza de azúcar','4 cucharadas de leche condensada'],
+    preparation:'Para empezar ponemos en la licuadora el hielo, los frutos rojos, las galletas, la Súper Leche Actilife Alpina, el Cremosino Alpina, el extracto de vainilla, el azúcar y licuamos hasta que todo quede muy bien integrado.'+
+    'Una vez listo nuestro batido, en un vaso para malteadas ponemos la leche condensada y servimos nuestro smoothie.',
+    image: 'https://my7.digitalexperience.ibm.com/1285e1d2-5151-4eab-9da2-775291879cb9/dxresources/d3da/d3daaa51-75c9-4b7a-ae2b-009a558790b3.png?resize=1400px%3A828px&crop=1400%3A500%3B0%2C164',
+    description: 'Este delicioso batido con sabor a cheesecake de moras es justo lo que necesitas para pasar una tarde agradable'
+  },
+  {
+    name: 'Manzanas con Yogurt Griego Alpina',
+    time: 10,
+    ingredients: ['2 manzanas', '1 cucharadita de limón', '2 cucharadas de maní', '½ cucharadita de canela','¼ de taza de azúcar morena','1 Yogurt Griego Alpina','1 cucharada de miel'],
+    preparation:'Con mucho cuidado, retiramos el centro de cada manzana.'+
+    'Retiramos las semillas y picamos el centro de las manzanas, Esto lo ponemos en un bowl y agregamos azúcar morena, canela, el maní picado y el zumo de limón.'+
+    'Mezclamos todo hasta que se integren los ingredientes. Con esta mezcla, rellenamos las manzanas y las llevamos al horno por 20 minutos a 180°'+
+    'En un bowl mezclamos el Yogurt Griego Alpina con la miel. Agregamos algunas cucharadas a cada manzana y a disfrutar.',
+    image: 'https://my7.digitalexperience.ibm.com/1285e1d2-5151-4eab-9da2-775291879cb9/dxresources/99b3/99b38c6f-cb27-42f0-b364-c9204df0533e.jpg?resize=1400px%3A788px&crop=1400%3A500%3B0%2C144',
+    description: '¿Pensando como sorprender a tus invitados para un brunch?. Esta receta arreglara tus mañanas'
+  },
+  {
+    name: 'Ensalada de frutas creativa con Queso Finesse',
+    time: 20,
+    ingredients: ['1 papayuela', '1 kiwi', '½ banano', '¼ de taza de fresas','½ mango','½ taza de Queso Finesse Deslactosado','¼ de taza de granola'],
+    preparation:'Lo primero que tenemos que hacer es partir la papayuela a la mitad, sacamos las pepitas y también sacamos un poquito de la papayuela formando un agujero en la mitad, aquí ya tenemos nuestro recipiente comestible.'+
+    'Ahora cortamos todos los demás ingredientes en láminas: kiwis, bananos, fresas, mango y el Queso Finesse Deslactosado.'+
+    'Tomamos las papayuelas y las llenamos con las frutas, aquí podemos ponernos un poco más creativos y organizar las frutas de manera que más nos guste, puede ser como filas de colores o formando espirales de frutas.'+
+    'Para finalizar ponemos un poco de granola por encima.'+
+    'Una variación muy rica de esta Ensalada de Frutas Creativa es llenar el centro de Yogurt Finesse Alpina del sabor que más te guste y ahí empezar a poner la granola con la fruta.',
+    image: 'https://my7.digitalexperience.ibm.com/1285e1d2-5151-4eab-9da2-775291879cb9/dxresources/a915/a9157935-8923-4307-8ab1-9b5ceedd107e.jpg?resize=1400px%3A789px&crop=1400%3A500%3B0%2C144',
+    description: 'Una ensalada de frutas con Queso Fresco Finesse Alpina dentro de una papayuela que sacará a la luz esa creatividad que llevas por dentro.'
+  },
+  {
+    name: 'Malteada de afán',
+    time: 10,
+    ingredients: ['1 Banano', '1 Manzana verde', '1 Taza de hielo', '1 Vaso de Avena Alpina','1 Bon Yurt'],
+    preparation:'Pelamos y picamos el banano y la manzana. En la licuadora mezclamos las frutas picadas, el hielo,  nuestra deliciosa Avena Alpina tradicional, el yogurt de un Bon Yurt y licuamos hasta integrar.'+
+    'Servimos en un vaso grande y finalizamos con las Zucaritas, ahora puedes disfrutar este rápido y nutritivo batido de afán, antes de que llegue el transporte.',
+    image: 'https://my7.digitalexperience.ibm.com/1285e1d2-5151-4eab-9da2-775291879cb9/dxresources/e593/e5930ca1-1188-4e88-aab8-fa00ce3eaf74.jpg?resize=1400px%3A800px&amp;crop=1400%3A500%3B0%2C150',
+    description: '¿No te despertó el reloj? Tranquilo Este rápido y nutritivo desayuno lo haces en menos de 5 minutos.'
+  },
+  {
+    name: 'Postre de Esponjado de Kumis',
+    time: 80,
+    ingredients: ['8 galletas negras', '2 cucharadas de Súper Leche Actilife Alpina', '2 vasos de Kumis Alpina', '2 cucharadas de gelatina sin sabor','1 cucharada de extracto de vainilla','150 ml de Crema de Leche Alpina batida','Dulce de moras'],
+    preparation:'En un procesador de alimentos vamos a poner las galletas negras sin crema con la Súper Leche Actilife Alpina, trituramos y reservamos.'+
+    'Luego de triturar las galletas, en un bowl vamos a poner el Kumis Alpina, la gelatina sin sabor, el extracto de vainilla, la Crema de Leche Alpina batida y mezclamos suavemente.'+
+    'Una vez lista nuestra mezcla, vamos a tomar un recipiente, servimos un poco y agregamos 2 cucharadas de dulce de moras a cada porción, el resto de crema la reservamos.'+
+    'El siguiente paso será elegir el recipiente para nuestras porciones, en cada uno de estos vamos a hacer una capa de galletas, una capa con la mezcla con dulce de moras y llevamos a la neverea por 20 minutos.'+
+    'Por último, sacamos de la nevera, ponemos otra capa con el resto de la crema que reservamos, llevamos a la nevera por otros 20 minutos. Una vez listo, decoramos con un poco de galleta negra y listo, a disfrutar.',
+    image: 'https://my7.digitalexperience.ibm.com/1285e1d2-5151-4eab-9da2-775291879cb9/dxresources/ecd2/ecd203c6-513c-4968-816d-b44ca141903d.png?resize=1400px%3A788px&crop=1400%3A500%3B0%2C144',
+    description: 'Prepara este postre de Esponjado de Kumis Alpina, es delicioso, fácil y rápido para endulzar una buena tarde con tus amigos con el mejor sabor de productos Alpina.'
+  },
+  {
+    name: 'Postrecito de capas',
+    time: 15,
+    ingredients: ['2 bananos', '2 Alpina Baby Papilla Trigo Miel', '1 cucharada de mango maduro picado'],
+    preparation:'Lo primero que haremos es picar los bananos y hacer un puré con ellos.'+
+    'Cuando ya lo tengamos listo buscamos el recipiente en el que vamos a servir y hacemos una capa de Alpina Baby Papilla Trigo Miel, luego otra con el puré de banano y así repetimos el proceso hasta tener el recipiente casi lleno.'+
+    'Por último, le agregamos el mango picado y ya tenemos nuestro postre listo en sencillos pasos.',
+    image: 'https://my7.digitalexperience.ibm.com/1285e1d2-5151-4eab-9da2-775291879cb9/dxresources/a6b5/a6b544fe-739b-448c-bb51-13446b2a9326.jpg?resize=1400px%3A788px&amp;crop=1400%3A500%3B0%2C144',
+    description: '¿Qué tal un delicioso postre de capas para tu bebé? disfruta de esta sencilla receta, sus colores y texturas le harán creer que está jugando mientras se alimenta.'
+  },
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
